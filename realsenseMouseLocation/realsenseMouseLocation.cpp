ccb2c823d0b52caf3bf00dcd73794c338b0dd089
puresox <puresox@163.com>
2021-01-05 15:48:23 +0800

    1.坐标转换 2.测试

--- realsenseMouseLocation/realsenseMouseLocation.cpp
+++ realsenseMouseLocation/realsenseMouseLocation.cpp
@@ -23,123 +23,130 @@ using namespace chrono;
 
 image_window win;
 
-auto getMouse2DPoint(rs_frame_image< dlib::rgb_pixel, RS2_FORMAT_RGB8 > &img)
+auto getMouse2DPoint(rs_frame_image< dlib::rgb_pixel, RS2_FORMAT_RGB8 >& img)
 {
-    try
+	try
 	{
-        std::array<std::tuple<int, int>, 21> points{};
-        // 创建人脸识别器
-        frontal_face_detector detector = get_frontal_face_detector();
+		std::array<std::tuple<int, int>, 21> points{};
+		// 创建人脸识别器
+		frontal_face_detector detector = get_frontal_face_detector();
 		// 创建并初始化人脸特征点识别器
 		shape_predictor sp;
 		deserialize("./shape_predictor_68_face_landmarks.dat") >> sp;
-        cout << "正在处理新图像..."<< endl;
-        // 显示图像
-        win.clear_overlay();
-        win.set_image(img);
-        // 人脸识别
+		cout << "正在处理新图像..." << endl;
+		// 显示图像
+		win.clear_overlay();
+		win.set_image(img);
+		// 人脸识别
 		auto start = system_clock::now();
-        std::vector<rectangle> dets = detector(img);
-        auto end = system_clock::now();
-        auto duration = duration_cast<microseconds>(end - start);
-        cout << "人脸识别花费了"
-            << double(duration.count()) * microseconds::period::num / microseconds::period::den
-            << "秒" << endl;
+		std::vector<rectangle> dets = detector(img);
+		auto end = system_clock::now();
+		auto duration = duration_cast<microseconds>(end - start);
+		cout << "人脸识别花费了"
+			<< double(duration.count()) * microseconds::period::num / microseconds::period::den
+			<< "秒" << endl;
 		cout << "检测到人脸数：" << dets.size() << endl;
-        if (dets.size() == 0)
-        {
-            return make_tuple(false, points);
-        }
-        // 从第一个人脸中识别特征点
-        full_object_detection shape = sp(img, dets[0]);
-        std::vector<full_object_detection> shapes;
+		if (dets.size() == 0)
+		{
+			return make_tuple(false, points);
+		}
+		// 从第一个人脸中识别特征点
+		full_object_detection shape = sp(img, dets[0]);
+		std::vector<full_object_detection> shapes;
 		cout << "共有特征点数：" << shape.num_parts() << endl;
-        // 输出嘴部20个特征点坐标以及中心坐标
-        int xCenterSum = 0, yCenterSum = 0;
-        for (int shapeNum = 48; shapeNum < 68; shapeNum++)
-        {
+		// 输出嘴部20个特征点坐标以及中心坐标
+		int xCenterSum = 0, yCenterSum = 0;
+		for (int shapeNum = 48; shapeNum < 68; shapeNum++)
+		{
 			cout << "第" << shapeNum << "个特征点位置：" << shape.part(shapeNum) << endl;
-            int partX = shape.part(shapeNum).x();
-            int partY = shape.part(shapeNum).y();
-            xCenterSum += partX;
-            yCenterSum += partY;
-            points[shapeNum-48] = make_tuple(partX, partY);
-        }
-        int xCenter = xCenterSum / 20, yCenter = yCenterSum / 20;
-        points[20] = make_tuple(xCenter, yCenter);
+			int partX = shape.part(shapeNum).x();
+			int partY = shape.part(shapeNum).y();
+			xCenterSum += partX;
+			yCenterSum += partY;
+			points[shapeNum - 48] = make_tuple(partX, partY);
+		}
+		int xCenter = xCenterSum / 20, yCenter = yCenterSum / 20;
+		points[20] = make_tuple(xCenter, yCenter);
 		cout << "嘴部中心位置：" << xCenter << "," << yCenter << endl;
 		// 展示特征点
-        shapes.push_back(shape);
-        win.add_overlay(render_face_detections(shapes));
-        return make_tuple(true, points);
-    }
-    catch (exception& e)
-    {
-        cout << "\nexception thrown!" << endl;
-        cout << e.what() << endl;
-    }
+		shapes.push_back(shape);
+		win.add_overlay(render_face_detections(shapes));
+		return make_tuple(true, points);
+	}
+	catch (exception& e)
+	{
+		cout << "\nexception thrown!" << endl;
+		cout << e.what() << endl;
+	}
+}
+
+void transformation(float point[3]) {
+	point[0] += 0;
+	point[1] += 0;
+	point[2] += 0;
 }
 
 int main(int argc, char* argv[]) try
 {
-    rs2::pipeline pipe;
-    pipe.start();
-    rs2::align align_to_color(RS2_STREAM_COLOR);
-    // 去掉开始前几秒的帧
-    for (int i = 0; i < 100; i++) rs2::frameset frames = pipe.wait_for_frames();
-    while (true)
-    {
-        rs2::frameset frames = pipe.wait_for_frames();
+	rs2::pipeline pipe;
+	pipe.start();
+	rs2::align align_to_color(RS2_STREAM_COLOR);
+	// 去掉开始前几秒的帧
+	for (int i = 0; i < 100; i++) rs2::frameset frames = pipe.wait_for_frames();
+	while (true)
+	{
+		rs2::frameset frames = pipe.wait_for_frames();
 		// 替换为对齐后的数据，不对其的话彩色图大小：640*480，深度图大小：848*480
-        frames = align_to_color.process(frames);
-        // 获取深度帧和彩色帧
-        rs2::depth_frame depth = frames.get_depth_frame();
-        rs2::video_frame vf = frames.get_color_frame();
-        // 保存彩色图像
-        rs_frame_image< dlib::rgb_pixel, RS2_FORMAT_RGB8 > image(vf);
-        // 从图像中识别嘴的坐标和距离
-        auto res = getMouse2DPoint(image);
-        auto succ = get<0>(res);
+		frames = align_to_color.process(frames);
+		// 获取深度帧和彩色帧
+		rs2::depth_frame depth = frames.get_depth_frame();
+		rs2::video_frame vf = frames.get_color_frame();
+		// 保存彩色图像
+		rs_frame_image< dlib::rgb_pixel, RS2_FORMAT_RGB8 > image(vf);
+		// 从图像中识别嘴的坐标和距离
+		auto res = getMouse2DPoint(image);
+		auto succ = get<0>(res);
 		auto& points = get<1>(res);
 		if (!succ)
-        {
-            cout << "未检测到人脸" << endl;
+		{
+			cout << "未检测到人脸" << endl;
 			continue;
-        }
-        float dist_to_mouse = 0.0;
-		for (int i = 20; (i >= 0)&&(dist_to_mouse==0.0); i--)
-        {
-			auto &point = points[i];
-            auto x = get<0>(point);
-            auto y = get<1>(point);
+		}
+		float dist_to_mouse = 0.0;
+		for (int i = 20; (i >= 0) && (dist_to_mouse == 0.0); i--)
+		{
+			auto& point = points[i];
+			auto x = get<0>(point);
+			auto y = get<1>(point);
 			dist_to_mouse = depth.get_distance(x, y);
-        }
-        auto x = get<0>(points[20]);
-        auto y = get<1>(points[20]);
+		}
+		auto x = get<0>(points[20]);
+		auto y = get<1>(points[20]);
 		if (dist_to_mouse == 0.0)
-        {
+		{
 			cout << "距离为0，重新检测" << endl;
 			continue;
 		}
-        cout << "距离嘴" << dist_to_mouse << "米远 " << endl;
-        // 坐标转换
-        float point[3];
-        float pixel[2]{ x,y };
-        rs2_intrinsics intr = frames.get_profile().as<rs2::video_stream_profile>().get_intrinsics();
-        rs2_deproject_pixel_to_point(point, &intr, pixel, dist_to_mouse);
-        // 输出三维坐标
+		cout << "距离嘴" << dist_to_mouse << "米远 " << endl;
+		// 坐标转换
+		float point[3];
+		float pixel[2]{ x,y };
+		rs2_intrinsics intr = frames.get_profile().as<rs2::video_stream_profile>().get_intrinsics();
+		rs2_deproject_pixel_to_point(point, &intr, pixel, dist_to_mouse);
+		transformation(point);
+		// 输出三维坐标
 		cout << "三维坐标计算完毕：" << point[0] << " , " << point[1] << " , " << point[2] << endl;
-    }
+	}
 	return EXIT_SUCCESS;
 }
 catch (const rs2::error& e)
 {
-    cerr << "RealSense error calling " << e.get_failed_function() << "(" << e.get_failed_args() << "):\n    " << e.what() << endl;
-    return EXIT_FAILURE;
+	cerr << "RealSense error calling " << e.get_failed_function() << "(" << e.get_failed_args() << "):\n    " << e.what() << endl;
+	return EXIT_FAILURE;
 }
 catch (const exception& e)
 {
-    cerr << e.what() << endl;
-    return EXIT_FAILURE;
+	cerr << e.what() << endl;
+	return EXIT_FAILURE;
 }
 
--- test/test.cpp
+++ test/test.cpp
@@ -1,20 +1,126 @@
-﻿// test.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
-//
-
+﻿
+#include <dlib/image_processing/frontal_face_detector.h> // Dlib
+#include <dlib/image_processing.h>
+#include <dlib/gui_widgets.h>
+#include <dlib/image_io.h>
+#include <opencv2/opencv.hpp> // opencv
+#include <boost/filesystem.hpp>
 #include <iostream>
+#include <chrono>
 
-int main()
+using namespace dlib;
+using namespace std;
+using namespace chrono;
+using namespace cv;
+
+// 获取路径下的所有图片文件名
+void readFilenamesBoost(std::vector<string>& filenames, const string folder)
 {
-    std::cout << "Hello World!\n";
+	boost::filesystem::path directory(folder);
+	boost::filesystem::directory_iterator itr(directory), end_itr;
+	string current_file = itr->path().string();
+
+	for (; itr != end_itr; ++itr)
+	{
+		if (is_regular_file(itr->path()))
+		{
+			string filename = itr->path().filename().string(); // 获取图片文件名
+			//string filename = itr->path().string(); // 获取图片路径
+			filenames.push_back(filename);
+		}
+	}
 }
 
-// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
-// 调试程序: F5 或调试 >“开始调试”菜单
 
-// 入门使用技巧: 
-//   1. 使用解决方案资源管理器窗口添加/管理文件
-//   2. 使用团队资源管理器窗口连接到源代码管理
-//   3. 使用输出窗口查看生成输出和其他消息
-//   4. 使用错误列表窗口查看错误
-//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
-//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
+// 在图像上画直线
+void drawLine(cv::Mat& img, int x1, int x2, int y1, int y2) {
+	cv::line(img, Point(x1, y1), Point(x2, y2), Scalar(0, 255, 0), 1);
+}
+
+// 在图像上画出嘴部
+void drawMouth(cv::Mat& img, const full_object_detection& shape) {
+	// 49-60 61-68
+	for (int shapeNum = 48; shapeNum < 60; shapeNum++)
+	{
+		int x1 = shape.part((shapeNum - 48) % 12 + 48).x();
+		int y1 = shape.part((shapeNum - 48) % 12 + 48).y();
+		int x2 = shape.part((shapeNum - 47) % 12 + 48).x();
+		int y2 = shape.part((shapeNum - 47) % 12 + 48).y();
+
+		drawLine(img, x1, x2, y1, y2);
+	}
+	for (int shapeNum = 60; shapeNum < 68; shapeNum++)
+	{
+		int x1 = shape.part((shapeNum - 60) % 8 + 60).x();
+		int y1 = shape.part((shapeNum - 60) % 8 + 60).y();
+		int x2 = shape.part((shapeNum - 59) % 8 + 60).x();
+		int y2 = shape.part((shapeNum - 59) % 8 + 60).y();
+
+		drawLine(img, x1, x2, y1, y2);
+	}
+}
+
+int main(int argc, char** argv)
+{
+	try
+	{
+		// 初始化
+		frontal_face_detector detector = get_frontal_face_detector();
+		shape_predictor sp;
+		deserialize("./shape_predictor_68_face_landmarks.dat") >> sp;
+
+		// 获取测试集所有图片
+		std::vector<string> filenames = {};
+		readFilenamesBoost(filenames, "./faces");
+		auto subStart = filenames.begin();
+		auto subEnd = filenames.begin() + 100;
+		std::vector<string> subVector(subStart, subEnd);
+		int filenum = subVector.size();
+		int correctnum = 0;
+
+		// 循环处理每一张图片
+		for (const string& filename : subVector)
+		{
+			cout << "processing image " << filename << endl;
+			array2d<unsigned char> dlibImg;
+			load_image(dlibImg, "./faces/" + filename);
+			while (dlibImg.nc() < 80 || dlibImg.nr() < 80)
+			{
+				//pyramid_up(dlibImg);
+			}
+
+			// 识别人脸
+			auto start = system_clock::now();
+			std::vector<dlib::rectangle> dets = detector(dlibImg);
+			auto end = system_clock::now();
+			auto duration = duration_cast<microseconds>(end - start);
+			cout << "人脸识别花费了"
+				<< double(duration.count()) * microseconds::period::num / microseconds::period::den
+				<< "秒" << endl;
+
+			// 没识别出来的继续下一轮
+			cout << "Number of faces detected: " << dets.size() << endl;
+			if (dets.size() == 0) continue;
+
+			// 识别人脸特征
+			full_object_detection shape = sp(dlibImg, dets[0]);
+			cout << "共有特征点数：" << shape.num_parts() << endl;
+
+			// 画出人脸特征
+			cv::Mat opencvImg;
+			opencvImg = cv::imread("./faces/" + filename);
+
+			drawMouth(opencvImg, shape);
+			cv::imwrite("./faces_render/" + filename, opencvImg);
+
+			correctnum++;
+		}
+		float rate = static_cast<float>(correctnum) / static_cast<float>(filenum);
+		cout << "准确率：" << rate * 100 << "%" << endl;
+	}
+	catch (exception& e)
+	{
+		cout << "\nexception thrown!" << endl;
+		cout << e.what() << endl;
+	}
+}
